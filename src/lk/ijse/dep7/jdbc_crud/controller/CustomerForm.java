ebecb0962b1fe878131f19ce4dfefe5aa607e1e7
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-07-30 12:51:42 +0530

    Finalize the CustomerForm with SS

--- src/lk/ijse/dep7/jdbc_crud/controller/CustomerForm.java
+++ src/lk/ijse/dep7/jdbc_crud/controller/CustomerForm.java
@@ -23,6 +23,7 @@ public class CustomerForm {
     public Button btnClear;
     public TextField txtSearch;
     private Connection connection;
+    private Statement stmSearch;
 
     public void initialize() {
         tblCustomers.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("id"));
@@ -51,11 +52,6 @@ public class CustomerForm {
         btnSave.setDisable(true);
         btnDelete.setDisable(true);
 
-        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
-
-            /* Todo: Find the appropriate customer(s) from the DB and list them in the table */
-        });
-
         tblCustomers.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, selectedCustomer) -> {
             if (selectedCustomer != null) {
                 txtID.setText(selectedCustomer.getId());
@@ -74,6 +70,7 @@ public class CustomerForm {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/dep7", "root", "mysql");
+            stmSearch = connection.createStatement();
         } catch (SQLException | ClassNotFoundException ex) {
             new Alert(Alert.AlertType.ERROR, "Failed to connect to the database server.").showAndWait();
             ex.printStackTrace();
@@ -106,6 +103,33 @@ public class CustomerForm {
             ex.printStackTrace();
         }
 
+        txtSearch.textProperty().addListener((observable, oldValue, newValue) -> {
+            try {
+
+                String sql = "SELECT * FROM customer WHERE id LIKE '%QUERY%' OR name LIKE '%QUERY%' OR address LIKE '%QUERY%' OR nic LIKE '%QUERY%';";
+                sql = sql.replaceAll("QUERY", newValue);
+//                String sql = "SELECT * FROM customer WHERE id LIKE '%%%1$s%%' OR name LIKE '%%%1$s%%' OR address LIKE '%%%1$s%%' OR nic LIKE '%%%1$s%%';";
+//                String sql = "SELECT * FROM customer WHERE id LIKE CONCAT('%%','%1$s','%%') OR name LIKE CONCAT('%%','%1$s','%%') OR address LIKE CONCAT('%%','%1$s','%%') OR nic LIKE CONCAT('%%','%1$s','%%');";
+//                sql = String.format(sql, newValue);
+//                System.out.println(sql);
+
+                ResultSet rst = stmSearch.executeQuery(sql);
+//                ResultSet rst = stmSearch.executeQuery("SELECT * FROM customer WHERE id LIKE '%" + newValue + "%' OR name LIKE '%" + newValue + "%' OR address LIKE '%" + newValue + "%' OR nic LIKE '%" + newValue + "%';");
+
+                tblCustomers.getItems().clear();
+                while (rst.next()) {
+                    String id = rst.getString("id");
+                    String nic = rst.getString("nic");
+                    String name = rst.getString(3);
+                    String address = rst.getString(4);
+                    tblCustomers.getItems().add(new CustomerTM(id, nic, name, address));
+                }
+            } catch (SQLException e) {
+                new Alert(Alert.AlertType.ERROR, "Failed to fetch the data, try again").show();
+                e.printStackTrace();
+            }
+        });
+
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
@@ -118,7 +142,23 @@ public class CustomerForm {
 
             try {
                 Statement stm = connection.createStatement();
-                String sql = "INSERT INTO customer VALUES ('%s','%s','%s','%s')";
+                String sql;
+
+                sql = "SELECT id FROM customer WHERE id='" + id + "'";
+                if (stm.executeQuery(sql).next()) {
+                    new Alert(Alert.AlertType.ERROR, "Customer ID already exists").show();
+                    txtID.requestFocus();
+                    return;
+                }
+
+                sql = "SELECT nic FROM customer WHERE nic='" + nic + "'";
+                if (stm.executeQuery(sql).next()) {
+                    new Alert(Alert.AlertType.ERROR, "NIC already exists").show();
+                    txtNIC.requestFocus();
+                    return;
+                }
+
+                sql = "INSERT INTO customer VALUES ('%s','%s','%s','%s');";
                 sql = String.format(sql, id, nic, name, address);
                 int affectedRows = stm.executeUpdate(sql);
 
@@ -136,24 +176,41 @@ public class CustomerForm {
 //                    case 1062:
 //                        errMsg = "Duplicate record, please check the customer ID and NIC";
 //                }
-                new Alert(Alert.AlertType.ERROR, ex.getMessage()).show();
+                new Alert(Alert.AlertType.ERROR, "Failed to save the customer, retry").show();
             }
         } else {
 
-            /* Todo: Update the customer in DB */
+            try {
 
-            if (true) {
-                CustomerTM selectedCustomer = tblCustomers.getSelectionModel().getSelectedItem();
+                Statement stm = connection.createStatement();
+                String sql = "SELECT * FROM customer WHERE nic = '" + nic + "';";
+                ResultSet rst = stm.executeQuery(sql);
+                //String sql = "SELECT * FROM customer WHERE nic = '"+ nic + "' AND id <> '" + id +"' ;";
+                if (rst.next() && !rst.getString("id").equals(id)) {
+                    new Alert(Alert.AlertType.ERROR, "NIC already exists").show();
+                    txtNIC.requestFocus();
+                    return;
+                }
 
-                selectedCustomer.setNic(nic);
-                selectedCustomer.setName(name);
-                selectedCustomer.setAddress(address);
-                tblCustomers.refresh();
-                btnClear.fire();
-            } else {
-                new Alert(Alert.AlertType.ERROR, "Failed to update the customer").show();
+                sql = "UPDATE customer SET name='%s', address='%s', nic='%s' WHERE id='%s'";
+                sql = String.format(sql, name, address, nic, id);
+                int affectedRows = stm.executeUpdate(sql);
+//                System.out.println(affectedRows);
 
-                /* Todo: Transfer the focus to right text field */
+                if (affectedRows == 1) {
+                    CustomerTM selectedCustomer = tblCustomers.getSelectionModel().getSelectedItem();
+
+                    selectedCustomer.setNic(nic);
+                    selectedCustomer.setName(name);
+                    selectedCustomer.setAddress(address);
+                    tblCustomers.refresh();
+                    btnClear.fire();
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Failed to update the customer, retry").show();
+                }
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+                new Alert(Alert.AlertType.ERROR, "Failed to update the customer, retry").show();
             }
 
         }
@@ -162,14 +219,20 @@ public class CustomerForm {
     public void btnDelete_OnAction(ActionEvent actionEvent) {
         CustomerTM selectedCustomer = tblCustomers.getSelectionModel().getSelectedItem();
 
-        // Todo: Delete the customer in DB
+        try {
+            Statement stm = connection.createStatement();
+            int affectedRows = stm.executeUpdate("DELETE FROM customer WHERE id='" + selectedCustomer.getId() + "'");
 
-        if (true) {
-            tblCustomers.getItems().remove(selectedCustomer);
-            tblCustomers.refresh();
-            btnClear.fire();
-        } else {
-            new Alert(Alert.AlertType.ERROR, "Failed to delete the customer").show();
+            if (affectedRows == 1) {
+                tblCustomers.getItems().remove(selectedCustomer);
+                tblCustomers.refresh();
+                btnClear.fire();
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Failed to delete the customer, retry").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to delete the customer, retry").show();
         }
     }
 
